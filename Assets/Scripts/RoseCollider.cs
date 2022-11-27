8b455fd1a2b9d15614b3812b8941f29fe0f6479b
LukeHoney <2248102@swansea.ac.uk>
2022-11-27 15:26:35 +0000

    Added more lighting and rose interaction

---
+++ Assets/Scripts/RoseCollider.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoseCollider : MonoBehaviour
+{
+    
+    public GameObject cheers;
+    public GameObject cheers1;
+    public GameObject cheers2;
+    public GameObject cheers3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cheers.SetActive(false);
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Rose")
+        {
+            Debug.Log("Rose hit singer");
+            cheers.SetActive(true);
+            cheers1.SetActive(true);
+            cheers2.SetActive(true);
+            cheers3.SetActive(true);
+        }
+    }
+}
