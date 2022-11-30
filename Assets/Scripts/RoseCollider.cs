a8d8a6ab6ab38b80500f9223b1ba1d8b97c9e737
LukeHoney <2248102@swansea.ac.uk>
2022-11-30 15:20:19 +0000

    Partical Effect added and rebaking of lights and reflections

--- Assets/Scripts/RoseCollider.cs
+++ Assets/Scripts/RoseCollider.cs
@@ -6,14 +6,17 @@ public class RoseCollider : MonoBehaviour
 {
     
     public GameObject cheers;
-    public GameObject cheers1;
-    public GameObject cheers2;
-    public GameObject cheers3;
+    //public GameObject cheers1;
+    //public GameObject cheers2;
+    //public GameObject cheers3;
+
+    public ParticleSystem petalParticles;
 
     // Start is called before the first frame update
     void Start()
     {
         cheers.SetActive(false);
+        petalParticles.Stop();
     }
 
     
@@ -24,9 +27,10 @@ public class RoseCollider : MonoBehaviour
         {
             Debug.Log("Rose hit singer");
             cheers.SetActive(true);
-            cheers1.SetActive(true);
-            cheers2.SetActive(true);
-            cheers3.SetActive(true);
+            //cheers1.SetActive(true);
+            //cheers2.SetActive(true);
+            //cheers3.SetActive(true);
+            petalParticles.Play();
         }
     }
 }
